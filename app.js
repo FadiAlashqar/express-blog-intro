4dfeb5ce3489cbb03c0ef1cd561b60ac15da9108
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-04-16 23:28:59 +0200

    [UPD] first step done

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello fadi');
+// importiamo express:
+const express = require('express');
+
+// inizializzo express all'interno di una variabile:
+const app = express();
+
+// definisco il numero di rotta:
+const port = 3000;
+
+// definizione della rotta base:
+app.get('/', (req, res) => {
+    res.send('Server del mio blog world');
+});
+
+
+app.listen(port, () => {
+    console.log(`in ascolto dalla base ${port}`)
+});
+
+
+
