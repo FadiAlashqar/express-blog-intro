454787c8d218505da23ac5a0dd7730952bfaf586
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-04-17 00:04:32 +0200

    [UPD] array done

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ const port = 3000;
 // definizione della rotta base:
 app.get('/', (req, res) => {
     res.send('Server del mio blog world');
+    let blog = [
+        {
+            titolo: "Ferrari LaFerrari",
+            contenuto: "contenuto 1",
+            immagine:'<img src="Ferrari Laferrari.jpg">',
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            titolo: "Lamborghini SVJ",
+            contenuto: "contenuto 2",
+            immagine: '<img src="lamborghini svj.jpg">',
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            titolo: "Mclaren P1",
+            contenuto: "contenuto 3",
+            immagine: '<img src="MclarenP1.avif">',
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            titolo: "Porsche 918",
+            contenuto: "contenuto 4",
+            immagine: '<img src="porsche 918.webp">',
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            titolo: "Bugatti Shiron",
+            contenuto: "contenuto 5",
+            immagine: '<img src="bugatti shiron.webp">',
+            tags: ["tag1", "tag2", "tag3"]
+        },
+    ]
 });
 
 
